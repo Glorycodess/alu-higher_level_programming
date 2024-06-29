a14f3033281b029c48f5db7bf8b4305ee88e2ea3
Glorycodess <g.paul@alustudent.com>
2024-06-29 10:14:24 -0700

    7-main.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,3 +1,5 @@
+#!/bin/usr/python3
+def add_tuple(tuple_a=(), tuple_b=()):
 # Ensure tuple_a has at least two elements, filling with 0 if necessary
     if len(tuple_a) < 2:
         tuple_a += (0, 0)
