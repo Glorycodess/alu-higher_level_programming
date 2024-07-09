747bff92ce2f336b99eb5350e7b48b76b9350921
Glorycodess <g.paul@alustudent.com>
2024-07-09 09:20:40 -0700

    2-main.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 
-def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    elif idx > len(my_list) - 1:
-        return my_list
-
-    for i in range(len(my_list)):
-        if i == idx:
-            my_list[i] = element
-            return my_list
+def safe_print_list_integers(my_list=[], x=0):
+    num = 0
+    for item in range(x):
+        try:
+            print("{:d}".format(my_list[item]), end='')
+            num += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return num
