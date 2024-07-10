a2012061d19bf5d1f69b87802fbc08e6aafb78b0
Glorycodess <g.paul@alustudent.com>
2024-07-10 04:24:57 -0700

    1-main.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""square module."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """
+        Initializes a square.
+
+        Args:
+            size: length of the side of the square.
+        """
+        self.__size = size
