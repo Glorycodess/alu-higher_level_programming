54138f4bea23741b71e2bf50ed6145ebe39c7c66
Glorycodess <g.paul@alustudent.com>
2024-07-10 04:12:01 -0700

    0-main.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""Square module."""
+
+
 class Square:
+    """Defines a square."""
     pass
+
+
+if __name__ == "__main__":
+    mysquare = Square()
+    print(type(mysquare))
+    print(mysquare.__dict__)
