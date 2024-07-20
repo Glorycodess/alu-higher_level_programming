1cede11f1b15cb9cf26fabf578138c0db86adfbe
Glorycodess <g.paul@alustudent.com>
2024-07-19 18:19:59 -0700

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
--- Correct output: user_0d_1 root user
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- lists all privileges of MySQL users user_0d_1 and user_0d_2
 
--- Correct output: user_0d_2 SELECT and INSERT privileges on the database user_2_db
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
