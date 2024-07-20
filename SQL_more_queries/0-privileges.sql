8cf6c958b570571199a46d4f6322fe5e716aff73
Glorycodess <g.paul@alustudent.com>
2024-07-19 17:44:52 -0700

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
--- Show grants for user_0d_1 and user_0d_2
+-- Correct output: user_0d_1 root user
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Correct output: user_0d_2 SELECT and INSERT privileges on the database user_2_db
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
